711ccc3c97cd1a853519a22c4e5df37f276babe8
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-12 21:49:28 -0300

    Using, StreamWriter

--- FileFileIO/Program.cs
+++ FileFileIO/Program.cs
@@ -35,31 +35,120 @@ namespace FileFileIO
             //}
             #endregion
 
-            #region FileStream StreamReader StreamWriter
+            #region FileStream StreamReader
 
-            string path = @"D:\Temp\file1.txt";
+            //string path = @"D:\Temp\file1.txt";
 
-            FileStream fileStream = null;
-            StreamReader streamReader = null;
+            //FileStream fileStream = null;
+            //StreamReader streamReader = null;
+
+            //try
+            //{
+            //    //fileStream = new FileStream(path, FileMode.Open);
+
+            //    //streamReader = new StreamReader(fileStream);
+
+            //    streamReader = File.OpenText(path);
+
+            //    while (!streamReader.EndOfStream)
+            //    {
+            //    string line1 = streamReader.ReadLine();
+            //    Console.WriteLine($"{line1}");
+
+            //    }
+
+            //    //string line2 = streamReader.ReadLine();
+
+            //    //Console.WriteLine(line2);
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    if (streamReader != null) streamReader.Close();
+            //    if (fileStream != null) fileStream.Close();
+
+            //}
+
+
+            #endregion
+
+            #region using
+
+
+            //string path = @"D:\Temp\file1.txt";
+
+
+            //try
+            //{
+            //    using (FileStream fileStream = new FileStream(path, FileMode.Open))
+            //    {
+            //        using (StreamReader streamReader = new StreamReader(fileStream))
+            //        {
+
+            //            while (!streamReader.EndOfStream)
+            //            {
+            //                string text = streamReader.ReadLine();
+            //                Console.WriteLine(text);
+            //            }
+            //        }
+
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+            //try
+            //{
+            //    using (StreamReader streamReader = File.OpenText(path))
+            //    {
+
+            //        while (!streamReader.EndOfStream)
+            //        {
+            //            string text = streamReader.ReadLine();
+            //            Console.WriteLine(text);
+            //        }
+            //    }
+
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            #endregion
+
+            #region StreamWriter
+
+            string sourcePath = @"D:\Temp\file1.txt";
+            string targetPath = @"D:\Temp\file2.txt";
 
             try
             {
-                //fileStream = new FileStream(path, FileMode.Open);
+                string[] lines = File.ReadAllLines(sourcePath);
 
-                //streamReader = new StreamReader(fileStream);
+                //File.WriteAllLines(targetPath, lines);
 
-                streamReader = File.OpenText(path);
-
-                while (!streamReader.EndOfStream)
+                using (StreamWriter streamWriter = File.AppendText(targetPath))
                 {
-                string line1 = streamReader.ReadLine();
-                Console.WriteLine($"{line1}");
-
+                    streamWriter.WriteLine("\n");
+                    foreach (string line in lines)
+                    {
+                        streamWriter.WriteLine($"{line.ToUpper()}");
+                    }
+                    
+                    Console.WriteLine("Done!");
                 }
-
-                //string line2 = streamReader.ReadLine();
-
-                //Console.WriteLine(line2);
 
             }
             catch (Exception ex)
@@ -67,15 +156,9 @@ namespace FileFileIO
 
                 Console.WriteLine(ex.Message);
             }
-            finally
-            {
-                if (streamReader != null) streamReader.Close();
-                if (fileStream != null) fileStream.Close();
-                
-            }
-
 
             #endregion
+
 
 
             Console.ReadKey();
